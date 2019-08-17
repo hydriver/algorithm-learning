e0f33afef21065ab9ccaf1cd66c6d056c1ff6b9e
rivers <rivers_hyd@163.com>
2019-08-17 17:44:12 +0800

    add Hello

---
+++ src/start/Hello.java
@@ -0,0 +1,21 @@
+package start;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("ok");
+        System.out.println("");
+
+//        int N = Integer.parseInt(args[0]);
+//        double lo = Double.parseDouble(args[1]);
+//        double hi = Double.parseDouble(args[2]);
+//        for (int i = 0; i < N; i++) {
+//            double x = StdRandom.uniform(lo, hi);
+//            StdOut.printf("%.2f\n", x);
+//        }
+    }
+
+    public static int rank(int[] a) {
+        return 0;
+    }
+}
