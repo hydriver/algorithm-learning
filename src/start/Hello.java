0af8fe0049cad346c988a365f16d718278712f66
rivers <rivers_hyd@163.com>
2019-08-17 17:50:17 +0800

    add some var

--- src/start/Hello.java
+++ src/start/Hello.java
@@ -6,12 +6,12 @@ public class Hello {
         System.out.println("ok");
         System.out.println("");
 
-//        int N = Integer.parseInt(args[0]);
-//        double lo = Double.parseDouble(args[1]);
-//        double hi = Double.parseDouble(args[2]);
+        int N = Integer.parseInt(args[0]);
+        double lo = Double.parseDouble(args[1]);
+        double hi = Double.parseDouble(args[2]);
 //        for (int i = 0; i < N; i++) {
-//            double x = StdRandom.uniform(lo, hi);
-//            StdOut.printf("%.2f\n", x);
+//            double x = edu.princeton.cs.algs4.StdRandom.uniform(lo, hi);
+//            edu.princeton.cs.algs4.StdOut.printf("%.2f\n", x);
 //        }
     }
 
